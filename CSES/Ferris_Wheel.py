96376b9df1ff6e8907b4270af7e0f1764dc8c8cc
mehul-lega <mehulchattopadhyaypersonal@gmail.com>
2026-05-09 10:36:11 +0530

    ferris wheel

---
+++ CSES/Ferris_Wheel.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solve(d,arr):
+
+    arr.sort()
+    cnt = 0
+    i,j = 0,len(arr)-1
+
+    while (i <= j):
+        if arr[i] + arr[j] <= d:
+            cnt+=1
+            i+=1
+            j-=1
+        else:
+            cnt+=1
+            j-=1
+        
+    
+    print(cnt)
+
+n, max_allowed_weight = map(int, input().split())
+w8s = list(map(int, input().split()))
+
+solve(max_allowed_weight,w8s)
